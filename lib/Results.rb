9511c39912a94a09e13e1f261efe2509661f24e9
kealanheena <kealanheena@gmail.com>
2020-07-21 15:44:45 +0100

    Chore: add red grades to list of returned grades

--- lib/Results.rb
+++ lib/Results.rb
@@ -1,22 +1,29 @@
 class Results
+
   def format_grades(grades)
-    result = ''
     grades_array = grades.split(", ")
     if grades_array.include?('Green')
-      grades_num = grade_counter(grades_array, "Green")
-      result = "Green: #{grades_num}"
+      green_grades = grade_counter(grades_array, "Green")
+      result = "Green: #{green_grades}"
     end
     if grades_array.include?('Amber')
-      grades_num = grade_counter(grades_array, "Amber")
-      if result != ''
-        result = result + "\nAmber: #{grades_num}"
+      amber_grades = grade_counter(grades_array, "Amber")
+      if result
+        result = result + "\nAmber: #{amber_grades}"
       else
-        result = "Amber: #{grades_num}"
+        result = "Amber: #{amber_grades}"
       end
     end
     if grades_array.include?('Red')
-      result = "Red: #{grades_array.length}"
-    elsif !grades_array.include?('Green') && !grades_array.include?('Amber')
+      red_grades = grade_counter(grades_array, "Red")
+      if result
+        result = result + "\nRed: #{red_grades}"
+      else
+        result = "Red: #{red_grades}"
+      end
+    end
+
+    if !grades_array.include?('Green') && !grades_array.include?('Amber') && !grades_array.include?('Red')
       result = "Uncounted: #{grades_array.length}"
     end
 
--- spec/Results_spec.rb
+++ spec/Results_spec.rb
@@ -48,10 +48,13 @@ describe Results do
       end
     end
 
-    context "if differnt grades are passed" do
-      it "should return 'Green: 1 \nAmber 1' when 'Green, Amber' is passed" do
+    context "if multiple types of grades are passed" do
+      it "should return 'Green: 1 \nAmber: 1' when 'Green, Amber' is passed" do
         expect(subject.format_grades('Green, Amber')).to eq("Green: 1\nAmber: 1")
+      end
 
+      it "should return 'Amber: 1 \nRed: 1' when 'Amber, Red' is passed" do
+        expect(subject.format_grades('Amber, Red')).to eq("Amber: 1\nRed: 1")
       end
     end
 
